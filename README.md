16afaa21df3d51102bd4e2ed6f6e3c7c2cfbddaa
zhangyunpeng <zhangyunpeng0126@xyz.cn>
2022-01-28 17:19:08 +0800

    project create

---
+++ README.md
@@ -0,0 +1,40 @@
+# FunctionPipeline
+
+A lib offer good ways for function programing in javascript (node and browser)
+
+## demo
+situation 1:two unrelated request
+```tsx
+const getArticeDetailById = (articleId) => {
+    return axios.get({url: '/api/comments/get', params: {articleId}})
+}
+
+const getDetailId = (res) => {
+    return res.data.id
+}
+
+const filterArticleFromResponse = (res) => {
+    return res.data.article
+}
+
+const setDetailInfo = (article) => {
+    setArticle(article)
+}
+
+const getCommoentsByArticleId = (articleId) => {
+    return axios.get({url: '/api/comments/get', params: {articleId}})
+}
+
+const hadndleArticleItemClick = (articleId) => {
+    return new FunctionPipe()
+        .next(getArticeDetailById(articleId))
+        .next(filterArticleFromResponse)
+        .next(setDetailInfo)
+        .next(getCommoentsByArticleId(articleId))
+}
+
+```
+
+## roadMap
+1. order control
+2. promise support
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+// import  EventEmitter from "EventEmitter"
+class FunctionPipeline {
+
+    constructor() {
+        // this.em = new EventEmitter()
+    }
+
+    private fns: Function[] = []
+    private fnsGroup: Function[][] = []
+    private promises: Promise<any>[] = []
+    private promisesCount = 0
+
+    private isPromise(obj: any): boolean {
+        return obj && obj instanceof Promise
+    }
+
+    public next(fn: Promise<any> | Function) {
+        if (this.isPromise(fn)) {
+            if (this.promises.length) {
+                this.fnsGroup[this.promisesCount++] = this.fns
+                this.fns = []
+            }
+            this.promises.push(fn as Promise<any>)
+        } else {
+            this.fns.push(fn as Function)
+        }
+        return this
+    }
+
+    public after(){
+
+    }
+
+    public run() {
+        this.promises.forEach((item, index) => {
+            item.then(res => {
+                this.fnsGroup[index].reduce((prev, curr) => prev ? curr(prev(res)) : curr(res))
+            })
+        })
+    }
+}
+
+export default FunctionPipeline
