b5c6ba59634eace527fc1ca8251af536b8415262
ZYP <694534942@qq.com>
2022-02-10 23:27:07 +0800

    [add]:implement promise order situation and test

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 // import  EventEmitter from "EventEmitter"
-enum Tag {
-    next = 'next',
-    after = 'after'
-}
+// enum Tag {
+//     next = 'next',
+//     after = 'after'
+// }
 
 type FnGroups = Function[][]
-type PromiseItem = { fn: Promise<any>, tag: string }
+type PromiseItem = { fn: Promise<any> }
 
 class FunctionPipeline {
 
     constructor() {
         // this.em = new EventEmitter()
+        console.log('FunctionPipeline is newing')
     }
 
     private fns: Function[] = []
@@ -24,11 +25,12 @@ class FunctionPipeline {
 
     public next(fn: Promise<any> | Function) {
         if (this.isPromise(fn)) {
+            console.log('this.fns,', this.fns)
             if (this.promises.length && this.fns.length) {
                 this.fnsGroup[this.promisesCount++] = this.fns
                 this.fns = []
             }
-            this.promises.push({fn: fn as Promise<any>, tag: Tag.next})
+            this.promises.push({fn: fn as Promise<any>})
         } else {
             this.fns.push(fn as Function)
         }
@@ -45,47 +47,81 @@ class FunctionPipeline {
     //     return this
     // }
 
-    private isExistCommonFunction(fnsGroup: FnGroups) {
-        return fnsGroup.length > 0
-    }
+    // private isExistCommonFunction(fnsGroup: FnGroups) {
+    //     return fnsGroup.length > 0
+    // }
 
-    private isLastPromise(index: number) {
-        return index === this.promises.length - 1
-    }
+    // private isLastPromise(index: number) {
+    //     return index === this.promises.length - 1
+    // }
 
     private fnExcute(arr: Function[], promiseRes: any) {
+       // console.log('arr', arr)
         arr.reduce((prev, curr, index) => {
-            if (prev && this.isPromise(prev)) {
-                prev.then((prevRes: any) => {
-                    // curr(prevRes)
-                    if (index + 1 < arr.length) {
-                        this.fnExcute(arr.slice(index + 1, arr.length), promiseRes)
-                    } else {
-                        curr(prevRes)
-                    }
-                })
+            console.log('prev', prev)
+            console.log('curr', curr)
+
+            if (index === 0) {
+                return curr(promiseRes)
             } else {
-                return prev ? curr(prev) : curr(promiseRes)
+                if (this.isPromise(prev)) {
+                    console.log('index + 1',index + 1)
+                    const restFns = arr.slice(index, arr.length)
+                    console.log('arr',arr)
+                    console.log('restFns',restFns)
+                    arr = []
+                    //@ts-ignore
+                    prev.then(prevRes => {
+                        this.fnExcute(restFns, prevRes)
+                    })
+                } else {
+                    return curr(prev)
+                }
             }
-        })
+
+        }, [])
     }
 
     public run() {
+        if (this.fns.length && !this.fnsGroup.length) {
+            this.fnsGroup[0] = this.fns
+        }
+        // console.log('this.promises', this.promises)
+        // console.log('this.fnsGroup', this.fnsGroup)
         if (this.promises.length) {
             this.promises.forEach((item, index) => {
-                if (item.tag === Tag.next) {
-                    item.fn.then(res => {
-                        if (!this.isLastPromise(index) && this.promises[index + 1].tag === Tag.after) {
-
-                        }
-                        this.isExistCommonFunction(this.fnsGroup) && this.fnExcute(this.fnsGroup[index])
-
-
-                    })
-                }
+                item.fn.then((res: any) => {
+                    console.log('this.fnsGroup.length', this.fnsGroup.length)
+                    // this.isExistCommonFunction(this.fnsGroup) && this.fnExcute(this.fnsGroup[index], res)
+                    index + 1 <= this.fnsGroup.length && this.fnExcute(this.fnsGroup[index], res)
+                    // this.fnsGroup[index].reduce((prev, curr, i) => {
+                    //     // console.log('prev',prev)
+                    //     console.log('i', i)
+                    //     console.log('prev', prev)
+                    //     if (i === 0) {
+                    //         return curr(res)
+                    //     } else {
+                    //         if (this.isPromise(prev)) {
+                    //             //@ts-ignore
+                    //             prev.then(prevRes => {
+                    //
+                    //             })
+                    //         }
+                    //         return curr(prev)
+                    //     }
+                    //     // return prev ? prev(res) :
+                    // }, [])
+                })
             })
         } else {
-            this.fns.reduce((prev, curr) => prev ? curr(prev()) : curr())
+            this.fns.reduce((prev, curr, index) => {
+                if (index === 0) {
+                    return curr()
+                } else {
+                    return curr(prev)
+                }
+
+            }, [])
         }
     }
 }
---
+++ test/index.test.js
@@ -0,0 +1,113 @@
+/**
+ * @title: index.test.ts
+ * @projectName functionPipeline
+ * @description: TODO
+ * @author zhangyunpeng0126
+ * @date 2022/1/2817:46
+ */
+//import FunctionPipeline from '../src';
+const FunctionPipeline = require('../dist/index')
+let storedArticle = {}
+// expect(sum(1, 1)).toEqual(2);
+const getArticeDetailById = (id) => {
+    // return axios.get({url:'/api/comments/get',params:{articleId}})
+    return new Promise((resolve => {
+        setTimeout(() => {
+            resolve({data: {id: 123, article: {title: 'jest good', content: 'good jest', authorId: 'abc'}}})
+        }, 200)
+    }))
+}
+
+const filterArticleFromResponse = (res) => {
+    return res.data.article
+}
+
+const setDetailInfo = (article) => {
+    console.log('article', article)
+    setArticle(article)
+}
+
+const setArticle = (article) => {
+    return storedArticle = article
+}
+
+const getCommoentsByArticleId = (id) => {
+    // return axios.get({url: '/api/comments/get', params: {articleId}})
+    return new Promise((resolve => {
+        setTimeout(() => {
+            resolve({data: {comments: []}})
+        }, 300)
+    }))
+}
+
+const filterAuthorId = (res) => {
+    return res.data.article.authorId
+}
+
+const getAuthorById = (id) => {
+    const map = {
+        'abc': '刘慈欣',
+        'asd': '阿斯顿'
+    }
+    return new Promise((resolve => {
+        setTimeout(() => {
+            resolve({data: {author: {name: map[id]}}})
+        }, 300)
+    }))
+}
+
+const hadndleArticleItemClickUnRelated = (articleId) => {
+    return new FunctionPipeline.default()
+        .next(getArticeDetailById(articleId))
+        .next(filterArticleFromResponse)
+        .next(setDetailInfo)
+        .next(getCommoentsByArticleId(articleId))
+        .run()
+}
+const hadndleArticleItemClickPromiseRelated = (articleId) => {
+    return new FunctionPipeline.default()
+        .next(getArticeDetailById(articleId))
+        .next(filterAuthorId)
+        .next(getAuthorById)
+        .next((name)=>{console.log('name',name)})
+        .run()
+}
+
+hadndleArticleItemClickUnRelated(123)
+
+
+// describe('promise unrelated', () => {
+//     it('works', () => {
+//
+//     });
+// });
+// describe('promise related', () => {
+//     it('works', () => {
+//         const getArticeDetailById = (articleId) => {
+//             // return axios.get({url:'/api/comments/get',params:{articleId}})
+//             return new Promise((resolve => {
+//                 setTimeout(() => {
+//                     resolve({data: {id: 123, article: {title: 'jest good', content: 'good jest'}}})
+//                 }, 200)
+//             }))
+//         }
+//
+//         const searchContentByTitle = (articleInfo) => {
+//             // return axios.get({url: '/api/comments/get', params: {articleId}})
+//             return new Promise((resolve => {
+//                 setTimeout(() => {
+//                     resolve({data: {}})
+//                 }, 300)
+//             }))
+//         }
+//
+//         const hadndleArticleItemClick = (articleId) => {
+//             return new FunctionPipeline()
+//                 .next(getArticeDetailById(articleId))
+//                 .next(searchContentByTitle)
+//                 .run()
+//         }
+//         hadndleArticleItemClick(211)
+//
+//     })
+// })
